7dab4d771f14c019d6c4f9dda321669b2803c070
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-07-31 18:49:05 +0800

    建立greeting.py講解函數是如何操作

---
+++ Python函數基本觀念/greeting.py
@@ -0,0 +1,17 @@
+# greeting.py
+
+
+def greeting():
+    """我的第一個Python 函數設計"""
+    print("Python歡迎你")
+    print("祝福學習順利")
+    print("謝謝")
+
+# 以下的程式碼也可稱為主程式
+
+
+greeting()
+greeting()
+greeting()
+greeting()
+greeting()
