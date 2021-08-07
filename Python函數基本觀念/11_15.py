e7ed9428f0ac9a3544869126921a0c535160c181
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-08-07 20:57:44 +0800

    更新2021_8_7_ 0857

---
+++ Python函數基本觀念/11_15.py
@@ -0,0 +1,13 @@
+# 11_15.py
+def guest_ifo(firstname,lastname,gender,middlename=''):
+    """整合客戶資料"""
+    if gender == "M":
+        welcome = lastname + middlename + firstname + '先生歡迎你'
+    else:
+        welcome = lastname + middlename + firstname + '小姐歡迎妳'
+    return welcome
+
+info1 = guest_ifo('濤','劉','M')
+info2 = guest_ifo('雨','洪','F','冰')
+print(info1)
+print(info2)
