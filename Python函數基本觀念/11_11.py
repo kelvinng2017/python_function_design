c4a788ac2fe068af7a6971217601fd570950a919
kelvinng2017 <kelvinngshuhui@gmail.com>
2021-08-07 13:38:18 +0800

    完成多個函數回傳至

---
+++ Python函數基本觀念/11_11.py
@@ -0,0 +1,9 @@
+# 11_11.py
+def subtract(x1,x2):
+    """減去設計"""
+    result = x1 -x2
+    return result
+print("本程式會執行a-b的運算")
+a = int(input("a = "))
+b = int(input("b = "))
+print("a - b =",subtract(a,b))
